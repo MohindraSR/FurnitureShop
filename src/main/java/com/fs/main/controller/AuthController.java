404a4e5992333d8a178a386c5fa268db07b5d249
MohindraSR <Mohindra.sr@alchemytechsol.com>
2026-01-23 17:41:59 +0530

    Add modification in apis and remove unused imports

--- src/main/java/com/fs/main/controller/AuthController.java
+++ src/main/java/com/fs/main/controller/AuthController.java
@@ -5,24 +5,17 @@ import com.fs.main.config.jwt.JwtUtil;
 import com.fs.main.service.CustomerService;
 import com.fs.main.entity.Customer;
 import jakarta.servlet.http.HttpServletResponse;
-import org.apache.tomcat.util.http.parser.Cookie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.time.Duration;
-import java.util.Collections;
-import java.util.Map;
-
 @RestController
 @RequestMapping("/api/auth/")
 public class AuthController {
--- src/main/java/com/fs/main/controller/FurnitureShopController.java
+++ src/main/java/com/fs/main/controller/FurnitureShopController.java
@@ -1,6 +1,5 @@
 package com.fs.main.controller;
 
-import com.fs.main.config.jwt.JwtUtil;
 import com.fs.main.dto.CategoryDto;
 import com.fs.main.dto.ProductDto;
 import com.fs.main.entity.Category;
@@ -9,9 +8,6 @@ import com.fs.main.service.CategoryService;
 import com.fs.main.service.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
